404c8d266a2158259c1bfbfa1ea570d3ec97ee7f
Fabián Astorga <astorgafabian6@gmail.com>
2019-03-07 18:04:40 -0600

    commit
    Actualiza codigo

--- src/components/square/index.js
+++ src/components/square/index.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default class Square extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
-    return <button className="square">{/* TODO: AQUI es JSX */}</button>;
+    return (
+      <button className="square" onClick={() => this.setState()}>
+        {this.state.value}
+      </button>
+    );
   }
 }
